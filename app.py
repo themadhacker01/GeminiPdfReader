434f1884dc96d11264c3128557664970ed189072
themadhacker01 <ameyshitole152@gmail.com>
2024-04-03 16:14:57 +0530

    Extracted text from pdf using PyPDF2

--- app.py
+++ app.py
@@ -16,3 +16,19 @@ load_dotenv()
 # Loads the gen ai API key
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key = GOOGLE_API_KEY)
+
+
+# Extract text from pdf with file path as input
+def get_pdf_text(file_path):
+    pdf_text = ''
+
+    # Open the pdf file with read permissions in binary format
+    with open(file_path, 'rb') as pdf_file:
+        pdf_reader = PdfReader(pdf_file)
+
+        # Iterate through each page in the pdf reader pages object
+        for pdf_page in pdf_reader.pages:
+            pdf_text += pdf_page.extract_text()
+
+        # Return the concatenated text from all pdf pages
+        return pdf_text
